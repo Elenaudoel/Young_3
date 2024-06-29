861967e91aa7fe3eac608e689bbc4e0df7204ab9
Elenaudoel <137452488+Elenaudoel@users.noreply.github.com>
2024-06-29 09:58:46 -0300

    tamo terminando

--- TCC/index.php
+++ TCC/index.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
 </head>
+
 <body>
     <form action="sessao.php" method="post">
         <h1 class="titulo-conteudo">
             Tela de Login
         </h1>
-        <div>
+        <div class="user-conteudo">
             <label for="user">Usuário</label>
             <input type="text" name="user" id="user">
         </div>
-        <div>
+        <div class="pass-conteudo">
             <label for="password">Senha</label>
             <input type="text" name="password" id="password">
         </div>
-        <div class="conteiner-btn">
-            <button type="submit">Entrar</button>
-            <button type="reset">Limpar</button>
+        <div class="btns">
+            <button class="enter" type="submit">Entrar</button>
+            <button class="sair" type="reset">Limpar</button>
         </div>
     </form>
 </body>
+
 </html>
--- TCC/painel.php
+++ TCC/painel.php
@@ -8,32 +8,39 @@ $resultado = $mysqli->query($sql);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>painel</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
-        <h1>Bem vindo <?php echo $nome_user;?></h1>
-        <p> <a href="logout.php">sair </a> </p>
-    
-    
-        <a> <?php if ($resultado->num_rows > 0) {
-            while ($linha = $resultado->fetch_assoc()) {
-                echo '<tr>';
-                echo '<a href="'.$linha['site'].'">' . $linha['nome']  . '</a>';
-                echo '<td>' . $linha['valor'] . '</td>';
-                echo '<img class="img_tenis" src="' . $linha['link'] . '">';
+
+    <h1>Bem vindo <?php echo $nome_user; ?></h1>
+    <p class="sair_btn"> <a href="logout.php">sair </a> </p>
+
+    <div class="main-content">
+
+        <div>
+            <?php if ($resultado->num_rows > 0) {
+                while ($linha = $resultado->fetch_assoc()) {
+                    echo '<div class="itens">';
+                    echo '<a href="' . $linha['site'] . '">' . $linha['nome']  . '</a>';
+                    echo '<td>' . $linha['valor'] . '</td>';
+                    echo '<img class="img_tenis" src="' . $linha['link'] . '">';
+                    echo '</div>';
+                }
             }
-        }
-        ?>
-  
-    </a>
+            ?>
+        </div>
+    </div>
+   
 
     <footer>
         <p>"Não concordo nem discordo, muito pelo contrário"</p>
     </footer>
 </body>
+
 </html>
